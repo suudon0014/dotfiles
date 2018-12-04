a9016f249c0f4ce5670d9a8cafe0da481962c093
Sudo <sudo@imv.co.jp>
2018-12-04 10:34:59 +0900

    add bat files to change env var for using vim and neovim

---
+++ setx_neovim.bat
@@ -0,0 +1,3 @@
+SETX /M VIM "C:\Neovim\bin"
+SETX /M VIMRUNTIME "C:\Neovim\share\nvim\runtime"
+pause
---
+++ setx_vim.bat
@@ -0,0 +1,3 @@
+SETX /M VIM "C:\Program Files (x86)\vim81-kaoriya-win64"
+SETX /M VIMRUNTIME "C:\Program Files (x86)\vim81-kaoriya-win64\vim81"
+pause
