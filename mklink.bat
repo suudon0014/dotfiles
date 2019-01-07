e59f1958ef1757139a327ee9b609560c324e1bb4
Sudo <sudo@imv.co.jp>
2019-01-07 09:50:32 +0900

    add cmd to create .vimrc and .gvimrc symlinks in mklink.bat file

--- mklink.bat
+++ mklink.bat
@@ -1,3 +1,9 @@
 cd nvim
 mklink init.vim %HOME%\dotfiles\.vimrc
 mklink ginit.vim %HOME%\dotfiles\.gvimrc
+
+cd %HOME%
+mklink .vimrc %HOME%\dotfiles\.vimrc
+mklink .gvimrc %HOME%\dotfiles\.gvimrc
+
+cd %HOME%\dotfiles
