3a065f47953d1b9270c817123b8e3ea0ec8910eb
sudo_home <m.sudo0104@gmail.com>
2019-12-21 16:06:39 +0900

    move vimrcs to ~/dotfiles/.vim/

--- mklink.bat
+++ mklink.bat
@@ -1,9 +1,9 @@
 cd nvim
-mklink init.vim %HOME%\dotfiles\.vimrc
-mklink ginit.vim %HOME%\dotfiles\.gvimrc
+mklink init.vim %HOME%\dotfiles\.vim\.vimrc
+mklink ginit.vim %HOME%\dotfiles\.vim\.gvimrc
 
 cd %HOME%
-mklink .vimrc %HOME%\dotfiles\.vimrc
-mklink .gvimrc %HOME%\dotfiles\.gvimrc
+mklink .vimrc %HOME%\dotfiles\.vim\.vimrc
+mklink .gvimrc %HOME%\dotfiles\.vim\.gvimrc
 
 cd %HOME%\dotfiles
