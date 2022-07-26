adebdf955763b500bfe0aa0f25ec2448801daf2e
suudon0014 <10444190+suudon0014@users.noreply.github.com>
2022-07-26 16:13:49 +0900

    add settings for sumneko_lua lsp

--- .vim/nvim-lsp.lua
+++ .vim/nvim-lsp.lua
@@ -37,6 +37,7 @@ mason.setup({
 
 local lspconfig = require("lspconfig")
 local mason_lspconfig = require("mason-lspconfig")
+
 mason_lspconfig.setup_handlers({ function(server_name)
     local opts = {}
 
@@ -49,6 +50,14 @@ mason_lspconfig.setup_handlers({ function(server_name)
             "--completion-style=detailed",
             "--header-insertion=never",
             "--limit-results=0",
+        }
+    end
+
+    if server_name == 'sumneko_lua' then
+        opts.settings = {
+            Lua = {
+                diagnostics = { globals = {'vim'} },
+            }
         }
     end
 
