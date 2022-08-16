732b3e57322f8d22061255d1347fb6c28847aa27
suudon0014 <10444190+suudon0014@users.noreply.github.com>
2022-08-16 11:56:36 +0900

    change the location of settings.json for windows-terminal to use one installed by scoop

--- install.sh
+++ install.sh
@@ -70,7 +70,7 @@ ln -sfv ${DOTFILES}/.zsh/.zshrc ~/.zshrc
 ln -sfv ${DOTFILES}/.fish/config.fish ${XDG_CONFIG_HOME}/fish/config.fish
 ln -sfv ${DOTFILES}/.fish/fish_prompt.fish ${XDG_CONFIG_HOME}/fish/functions/fish_prompt.fish
 
-ln -sfv ${DOTFILES}/WindowsTerminal/settings.json ${LOCALAPPDATA}/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json
+ln -sfv ${DOTFILES}/WindowsTerminal/settings.json "${LOCALAPPDATA}/Microsoft/Windows Terminal/settings.json"
 
 ln -sfv ${DOTFILES}/.latexmkrc ~/.latexmkrc
 
