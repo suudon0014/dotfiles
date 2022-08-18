4f3f6f1badb9c53456cfba4dd0281b209bb7e4fd
suudon0014 <10444190+suudon0014@users.noreply.github.com>
2022-08-18 12:24:33 +0900

    modify indents

--- .vim/lua/nvim-lsp.lua
+++ .vim/lua/nvim-lsp.lua
@@ -1,14 +1,14 @@
- vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
-   vim.lsp.handlers.hover, {
-     border = "single"
-   }
- )
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+    vim.lsp.handlers.hover, {
+        border = "single"
+    }
+)
 
- vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
-   vim.lsp.handlers.signature_help, {
-     border = "single"
-   }
- )
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
+    vim.lsp.handlers.signature_help, {
+        border = "single"
+    }
+)
 
 local saga = require('lspsaga')
 local action = require('lspsaga.action')
