08581cbe36df00ecce9e797abf1d7cc7496f04a0
suudon0014 <10444190+suudon0014@users.noreply.github.com>
2024-08-22 16:22:33 +0900

    feat: add some key configs for wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -11,11 +11,17 @@ config.keys = {
     -- {key = 'f', mods = 'CTRL', action = act.SendKey {key = 'f', mods = 'META',},},
     {key = 'f', mods = 'SHIFT|META', action = act.ToggleFullScreen,},
     {key = 'q', mods = 'SHIFT|CTRL', action = act.CloseCurrentTab {confirm = false},},
+    {key = 'r', mods = 'SHIFT|CTRL', action = act.ReloadConfiguration},
     {key = 's', mods = 'SHIFT|CTRL', action = act.SplitHorizontal {domain = 'CurrentPaneDomain'},},
     {key = 't', mods = 'SHIFT|CTRL', action = act.SpawnTab 'CurrentPaneDomain',},
     {key = 't', mods = 'META', action = act.ShowTabNavigator,},
+    {key = 'v', mods = 'CTRL', action = act.PasteFrom 'Clipboard'},
     {key = 'v', mods = 'SHIFT|CTRL', action = act.SplitVertical {domain = 'CurrentPaneDomain'},},
     {key = '[', mods = 'CTRL', action = act.PaneSelect,},
+    {key = 'h', mods = 'LEADER', action = act.AdjustPaneSize {'Left', 5}},
+    {key = 'j', mods = 'LEADER', action = act.AdjustPaneSize {'Down', 5}},
+    {key = 'k', mods = 'LEADER', action = act.AdjustPaneSize {'Up', 5}},
+    {key = 'l', mods = 'LEADER', action = act.AdjustPaneSize {'Right', 5}},
 }
 
 config.audible_bell = 'Disabled'
@@ -25,6 +31,7 @@ config.font = wezterm.font('Cica', {weight='Regular', stretch='Normal', style='N
 config.font_size = 12
 config.initial_cols = 100
 config.initial_rows = 50
+config.leader = {key = 'a', mods = 'CTRL'}
 config.window_background_opacity = 0.85
 config.window_close_confirmation = 'NeverPrompt'
 
