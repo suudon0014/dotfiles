06ac29e2041c742512832e752fd51e0f4b85f151
suudon0014 <10444190+suudon0014@users.noreply.github.com>
2025-03-27 15:00:28 +0900

    fix: delete deprecated configs for vim.lsp

--- .vim/lua/nvim-lsp.lua
+++ .vim/lua/nvim-lsp.lua
@@ -1,17 +1,5 @@
 require('vim.lsp.log').set_level('OFF')
 
-vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
-    vim.lsp.handlers.hover, {
-        border = "single"
-    }
-)
-
-vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
-    vim.lsp.handlers.signature_help, {
-        border = "single"
-    }
-)
-
 local saga = require('lspsaga')
 saga.setup({
     outline = {
