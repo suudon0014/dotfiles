ee905c8cc4a3dd15a70708843899cd1d00c4ef82
suudon0014 <10444190+suudon0014@users.noreply.github.com>
2025-12-17 11:56:46 +0900

    refactor: add function for the same procedure

---
+++ .vim/lua/utils.lua
@@ -0,0 +1,16 @@
+local M = {}
+
+function M.LoadTextFile(fileName)
+    local home_dir = os.getenv('HOME')
+    local prompt_dir = 'dotfiles/.vim/llm_prompt'
+    local filePath = home_dir .. '/' .. prompt_dir .. '/' .. fileName
+    local file = io.open(filePath, "r")
+    local prompt = ""
+    if file then
+        prompt = file:read("*a")
+        file:close()
+    end
+    return prompt
+end
+
+return M
