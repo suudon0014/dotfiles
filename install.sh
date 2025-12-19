d1c0e05997f24438890ffd977a01e63c9d8548a5
suudon0014 <10444190+suudon0014@users.noreply.github.com>
2025-12-19 10:00:56 +0900

    chore: remove unused configs

--- install.sh
+++ install.sh
@@ -13,41 +13,6 @@ if [ "$(expr substr $(uname -s) 1 4)" == "MSYS"\
      setx XDG_CONFIG_HOME ${XDG_CONFIG_HOME}
      setx MSYS winsymlinks:nativestrict
      setx MSYS2_PATH_TYPE inherit
-
-    # Neovim
-    # NeovimRoot=C:/Neovim
-    # setx VIM ${NeovimRoot}/share/nvim
-    # setx VIMRUNTIME ${NeovimRoot}/share/nvim/runtime
-
-    # Vim
-    # VimRoot=C:/Vim
-    # setx VIM ${VimRoot}
-    # setx VIMRUNTIME ${VimRoot}/vim82
-
-    # Create python venvs for neovim
-    # if [ ! -d ~/.venv ]; then
-    #     echo -e "\nPython venvs for neovim is not found.\nStart creating..."
-    #     mkdir ~/.venv && cd ~/.venv
-
-    #     # python2 venv for neovim
-    #     python -m virtualenv neovim2
-    #     source neovim2/Scripts/activate
-    #     pip install -U neovim
-    #     deactivate
-
-    #     # python3 venv for neovim
-    #     python3 -m venv neovim3 --without-pip
-    #     curl -kL https://bootstrap.pypa.io/get-pip.py > get-pip.py
-    #     source neovim3/Scripts/activate
-    #     python get-pip.py
-    #     pip install -U neovim python-language-server compiledb
-    #     deactivate
-
-    #     cd ~
-    # else
-    #     echo -e "\nPython venvs for neovim is found."
-    # fi
-
 # for Linux
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     export XDG_CONFIG_HOME=${XDG_CONFIG_HOME}
@@ -56,21 +21,20 @@ else
 fi
 
 DOTFILES=~/dotfiles
+
+# vim
 ln -sfv ${DOTFILES}/.vim/.vimrc ${XDG_CONFIG_HOME}/nvim/init.vim
 ln -sfv ${DOTFILES}/.vim/.gvimrc ${XDG_CONFIG_HOME}/nvim/ginit.vim
 ln -sfv ${DOTFILES}/.vim/.vimrc ~/.vimrc
 ln -sfv ${DOTFILES}/.vim/.gvimrc ~/.gvimrc
 
+# git
 ln -sfv ${DOTFILES}/.gitconfig ~/.gitconfig
 ln -sfv ${DOTFILES}/.git_commit_template ~/.git_commit_template
 
+# etc.
 ln -sfv ${DOTFILES}/.zsh/.zshrc ~/.zshrc
-
-# ln -sfv ${DOTFILES}/.fish/config.fish ${XDG_CONFIG_HOME}/fish/config.fish
-# ln -sfv ${DOTFILES}/.fish/fish_prompt.fish ${XDG_CONFIG_HOME}/fish/functions/fish_prompt.fish
-
 ln -sfv ${DOTFILES}/.latexmkrc ~/.latexmkrc
-
 mkdir -p ${XDG_CONFIG_HOME}/lagygit
 ln -sfv ${DOTFILES}/lazygit/config.yml ${XDG_CONFIG_HOME}/lazygit/config.yml
 
@@ -82,3 +46,4 @@ ln -sfv ${DOTFILES}/lazygit/config.yml ${XDG_CONFIG_HOME}/lazygit/config.yml
 # ln -sfv ${SCOOP_DIR}/windows_terminal_settings.json ~/scoop/persist/windows-terminal/settings/settings.json
 # mkdir -p ${APPDATA}/bat
 # ln -sfv ${SCOOP_DIR}/bat_config ${APPDATA}/bat/config
+
