0218c8196b06ef764315c368a0efff5626ed1549
suudon0014 <10444190+suudon0014@users.noreply.github.com>
2026-01-06 11:52:59 +0900

    refactor(vim): migrate plugins for treesitter to lazy.nvim

---
+++ .vim/lua/plugins/treesitter.lua
@@ -0,0 +1,29 @@
+return {
+  {
+    'nvim-treesitter/nvim-treesitter',
+    branch = 'master',
+    event = { 'InsertEnter', 'BufReadPost', 'BufNewFile' },
+    cmd = { 'TSUpdate', 'TSUpdateSync' },
+    build = ':TSUpdate',
+    dependencies = {
+      'haringsrob/nvim_context_vt',
+      'm-demare/hlargs.nvim',
+      {
+        'Wansmer/treesj',
+        config = function()
+          require('treesj').setup({
+            use_default_keymaps = false,
+          })
+          vim.keymap.set('n', '<Leader>M', function()
+            require('treesj').toggle({ split = { recursive = true } })
+          end)
+        end,
+      },
+      -- 'nvim-treesitter/nvim-treesitter-textobjects',
+      -- 'RRethy/nvim-treesitter-textsubjects',
+    },
+    config = function()
+      vim.cmd.source(vim.fn.expand('~/dotfiles/.vim/lua/treesitter-setup.lua'))
+    end,
+  },
+}
---
+++ .vim/lua/treesitter-setup.lua
@@ -0,0 +1,104 @@
+local status_ok, config = pcall(require, "nvim-treesitter.config")
+if not status_ok then
+    return
+end
+
+config.setup {
+    ensure_installed = {
+        "bash", "c", "c_sharp", "cmake", "comment", "cpp", "diff", "dockerfile", "git_config", "git_rebase",
+        "gitattributes", "gitcommit", "gitignore", "html", "java", "javascript", "jsdoc", "json", "latex", "lua",
+        "luadoc", "make", "markdown", "markdown_inline", "matlab", "python", "r", "regex", "rust", "sql",
+        "todotxt", "toml", "tsx", "typescript", "vim", "vimdoc", "yaml",
+    },
+    sync_install = false,
+    auto_install = true,
+    highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = {"markdown"},
+
+        -- パフォーマンス改善: 大きなファイルではTreesitterを無効にする
+        disable = function(lang, buf)
+            local max_filesize = 10 * 1024 * 1024 -- 10 MB
+            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+            if ok and stats and stats.size > max_filesize then
+                return true
+            end
+        end,
+    },
+    indent = {
+        enable = true,
+    },
+    -- textobjects = {
+    --     select = {
+    --         enable = true,
+    --         lookahead = true,
+    --         keymaps = {
+    --             ["af"] = "@function.outer",
+    --             ["if"] = "@function.inner",
+    --             ["ac"] = "@class.outer",
+    --             ["ic"] = "@class.inner",
+    --             ["al"] = "@loop.outer",
+    --             ["il"] = "@loop.inner",
+    --         },
+    --         selection_modes = {
+    --             ['@parameter.outer'] = 'v',
+    --             ['@function.outer'] = 'v',
+    --             ['@class.outer'] = '<C-v>',
+    --             ['@loop.outer'] = 'v',
+    --         },
+    --     },
+    --     swap = {
+    --         enable = true,
+    --         swap_next = {
+    --             ["<Leader>n"] = {"@parameter.inner"},
+    --         },
+    --         swap_previous = {
+    --             ["<Leader>p"] = {"@parameter.inner"},
+    --         }
+    --     },
+    --     move = {
+    --         enable = true,
+    --         set_jumps = true,
+    --         goto_next_start = {
+    --             ["]m"] = "@function.outer",
+    --             ["]]"] = "@class.outer",
+    --         },
+    --         goto_next_end = {
+    --             ["]M"] = "@function.outer",
+    --             ["]["] = "@class.outer",
+    --         },
+    --         goto_previous_start = {
+    --             ["[m"] = "@function.outer",
+    --             ["[["] = "@class.outer",
+    --         },
+    --         goto_previous_end = {
+    --             ["[M"] = "@function.outer",
+    --             ["[]"] = "@class.outer",
+    --         },
+    --     },
+    --     lsp_interop = {
+    --         enable = true,
+    --         border = 'single',
+    --         peek_definition_code = {
+    --             ["<C-l>pf"] = "@function.outer",
+    --             ["<C-l>pc"] = "@class.outer",
+    --         },
+    --     },
+    -- },
+    -- textsubjects = {
+    --     enable = true,
+    --     prev_selection = ",",
+    --     keymaps = {
+    --         ['.'] = 'textsubjects-smart',
+    --         [';'] = 'textsubjects-container-outer',
+    --         ['i;'] = 'textsubjects-container-inner',
+    --     },
+    -- },
+}
+
+local hlargs_ok, hlargs = pcall(require, 'hlargs')
+if hlargs_ok then
+    hlargs.setup({
+        excluded_filetypes = {'toml'},
+    })
+end
