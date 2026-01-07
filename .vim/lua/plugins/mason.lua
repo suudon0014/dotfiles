fdb96cd78e54bf45c9ff1a4daef32f6207d193a7
suudon0014 <10444190+suudon0014@users.noreply.github.com>
2026-01-07 14:09:48 +0900

    feat(vim): remove ddc.vim, add blink.cmp, migrate settings to lazy.nvim

---
+++ .vim/lua/plugins/mason.lua
@@ -0,0 +1,41 @@
+return {
+  {
+    'mason-org/mason.nvim',
+    event = { 'CursorHold', 'InsertEnter', 'BufReadPost', 'BufNewFile', 'VeryLazy' },
+    cmd = { 'Mason', 'MasonInstall', 'MasonUninstall', 'MasonUninstallAll', 'MasonLog' },
+    dependencies = {
+      'mason-org/mason-lspconfig.nvim',
+      'neovim/nvim-lspconfig',
+      'glepnir/lspsaga.nvim',
+      'j-hui/fidget.nvim',
+      'saghen/blink.cmp',
+    },
+    config = function()
+      vim.cmd.source(vim.fn.expand('~/dotfiles/.vim/lua/nvim-lsp.lua'))
+    end,
+  },
+  {
+    'mason-org/mason-lspconfig.nvim',
+    lazy = true,
+  },
+  {
+    'neovim/nvim-lspconfig',
+    lazy = true,
+    config = function()
+      vim.api.nvim_create_autocmd({ 'FileType' }, {
+        pattern = { 'lspinfo' },
+        callback = function()
+          vim.keymap.set('n', 'q', vim.cmd.close, { remap = false, buffer = true, silent = true })
+        end,
+      })
+    end,
+  },
+  {
+    'glepnir/lspsaga.nvim',
+    lazy = true,
+  },
+  {
+    'j-hui/fidget.nvim',
+    lazy = true,
+  },
+}
