bee90169fc9a1ccc455698c8ee064d9d201d87d5
suudon0014 <10444190+suudon0014@users.noreply.github.com>
2026-08-17 11:18:20 +0900

    fix: add providers for obsidian.nvim plugin to avoid errors in opening markdown files

--- .vim/lua/plugins/cmp.lua
+++ .vim/lua/plugins/cmp.lua
@@ -52,6 +52,18 @@ return {
                     markdown = markdown_sources,
                 },
                 providers = {
+                    obsidian = {
+                        name = 'obsidian',
+                        module = 'blink.compat.source',
+                    },
+                    obsidian_new = {
+                        name = 'obsidian_new',
+                        module = 'blink.compat.source',
+                    },
+                    obsidian_tags = {
+                        name = 'obsidian_tags',
+                        module = 'blink.compat.source',
+                    },
                     lsp = { min_keyword_length = 1 },
                     buffer = { min_keyword_length = 1 },
                     skkeleton = {
